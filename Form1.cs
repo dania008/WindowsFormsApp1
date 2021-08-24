8e374bf7881e50c6997374b07e204109e8af56fd
даниил володин <dania00@bk.ru>
2021-08-24 21:33:43 +0300

    0.1

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        MyProcess myProcess = new MyProcess();
+        public Form1()
+        {
+            InitializeComponent();
+            myProcess.callEvent += setProgressBar;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myProcess.StartProcess();
+        }
+        public void setProgressBar(int val)
+        {
+            progressBar1.Value = val;
+        }
+    }
+    public class MyProcess
+    {
+        public delegate void Mydelegate(int par);
+        public event Mydelegate callEvent;
+        public void StartProcess()
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                callEvent(i);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
