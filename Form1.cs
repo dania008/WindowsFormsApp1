1ef783b88b643509550f0e1c1a8944649dc80b36
даниил володин <dania00@bk.ru>
2021-08-24 21:50:30 +0300

    0,2

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         // Comment
+        // Comment
         MyProcess myProcess = new MyProcess();
         public Form1()
         {
